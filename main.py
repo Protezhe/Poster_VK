c9977590c0f0c432da16b3e9c588ce9ad79d8dcb
protezhe <santamozzarella@gmail.com>
2024-01-31 14:01:53 +0300

    Посчитать

--- main.py
+++ main.py
@@ -92,11 +92,53 @@ bot = telebot.TeleBot(token_tg)
 bot.send_message('206172159', 'Бот запущен')
 
 
+def statistic(folder, tag):
+
+    count = 0
+
+    folder = folder + '_txt'
+
+    files = os.listdir(folder)
+
+    for item in files:
+
+        if item != '.DS_Store':
+
+            text_posta = linecache.getline(folder + '/' + item, 3)
+            linecache.clearcache()
+
+
+            if text_posta.find('#') != -1:
+
+                post_tag = text_posta.split('#') [1]
+
+                post_tag = post_tag.rstrip()
+
+                if post_tag.find('@') != -1:
+
+                    post_tag = post_tag.split('@') [0]
+
+                    post_tag = post_tag.rstrip()
+
+
+                if post_tag == tag:
+
+                    count = count + 1
+
+
+    return count
+
+
+
+
+
+
 #Специаьные функции
 def sprashivaem_za_fotku(folder, poisk_nomer_ocheredi):
 
     files = os.listdir(folder)
 
+    ochered = False
 
     random.shuffle(files)
 
@@ -145,6 +187,7 @@ def sprashivaem_za_video(folder, poisk_nomer_ocheredi):
 
     files = os.listdir(folder)
 
+    ochered = False
 
     random.shuffle(files)
 
@@ -280,6 +323,8 @@ def vk_poisk_upload_photo(folder, poisk_nomer_ocheredi, tag):
 
     files = os.listdir(folder)
 
+    ochered = False
+
 
     random.shuffle(files)
 
@@ -393,6 +438,7 @@ def vk_poisk_upload_video(folder, poisk_nomer_ocheredi, tag):
 
     files = os.listdir(folder)
 
+    ochered = False
 
     random.shuffle(files)
 
@@ -585,6 +631,41 @@ def raspisanie(m, res=False):
     print('Rasp_send')
     bot.send_document('206172159', document=open('rasp.txt', 'rb'))
 
+
+
+@bot.message_handler(commands=["stat"])
+def stat(m, res=False):
+
+    photo_selfie = statistic('photos', 'selfie')
+
+    photo_photo = statistic('photos', 'photo')
+
+    photo_friends = statistic('photos', 'friends')
+
+    photo_ava = statistic('photos', 'ava')
+
+    video_life = statistic('videos', 'life')
+
+    video_sport = statistic('videos', 'sport')
+
+    video_drama = statistic('videos', 'drama')
+
+    video_izobreteniya = statistic('videos', 'izobreteniya')
+
+    video_music = statistic('videos', 'music')
+
+    summa = 'Photo_selfei = ' + str(photo_selfie) + '\n' + 'photo_photo = ' + str(
+        photo_photo) + '\n' + 'photo_friends = ' + str(photo_friends) + '\n' + 'photo_ava = ' + str(
+        photo_ava) + '\n' + 'video_life = ' + str(video_life) + '\n' + 'video_sport = ' + str(
+        video_sport) + '\n' + 'video_drama = ' + str(video_drama) + '\n' + 'video_izobreteniya = ' + str(
+        video_izobreteniya) + '\n' + 'video_music = ' + str(video_music)
+
+
+    print(summa)
+
+    bot.send_message('206172159', summa)
+
+
 @bot.message_handler(commands=["vk_start"])
 def raspisanie(m, res=False):
     bot.send_message('206172159', 'Публикации вк запущены')
@@ -745,6 +826,7 @@ def handle_text(message):
 
         bot.send_message('206172159', 'Ништяк!')
 
+
     else:
 
         bot.send_message('206172159', 'Чего?')
@@ -832,6 +914,9 @@ def new_time():
     schedule.every().day.at('09:00').do(spros_za_fotku)
     schedule.every().day.at('20:00').do(spros_za_video)
 
+
+
+
     time.sleep(80)
 
     schedule.every().day.at('05:58').do(new_time)
@@ -848,7 +933,8 @@ def runBot():
 def runSchedulers():
 
 
-    schedule.every().day.at('05:58').do(new_time)
+    schedule.every().day.at('13:21').do(new_time)
+
 
 
     while True:
